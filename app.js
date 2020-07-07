e1e08218697cab4fe660f0ae83667cc4be35c404
Rohit Sharma <rsedwardian@gmail.com>
2020-07-08 03:13:29 +0530

    making changes, removing extra api call

--- app.js
+++ app.js
@@ -64,49 +64,6 @@ app.get('/pay-headless', async (req, res) => {
     // how to do a post - https://stackoverflow.com/questions/47060534/how-do-post-request-in-puppeteer
     //await page.setRequestInterception(true);
 
-    /*page.on('request', interceptedRequest => {
-
-        // Here, is where you change the request method and
-        // add your post data
-        var data = {
-            'method': 'POST',
-            'postData': 'PaReq=eNpVUttygjAQfc9XOH6AIaHgZdbMaHUGWnUcRWp9i5ARWgkaoKP9+iYotd2nPZvNyZ6zgSBRQkzWIqqUYDAXRcEPopXGw/bRec3ctxWnTjgTPdtT3oa0GSxHK3Fm8CVUkeaSkY7VoYAbiDSFihIuSwY8Oo/9BXuyidXXLXeIIBPKnzBCrSZsHYBvZQSSZ4KNFZdR0vJlKZTkpabmR8D1EYIor2Sprsx2XMANQFCpI0vK8lQMMN7X9ztRjgGbOgL8GGxZmazQei9pzPaTcLYPdttNNr0uwt18Fxy+196LHwbTIWDTgSDmpWDU0iO7lLSs/sCxBkRrqusIeGaGYKtCu+FopTeI4GQeGt0QMSd/C1pHpZSQUSOkQQjE5ZRLoXv0E7+5lvAY/Nkz/kaldqwbL7rXd+dz63zMe8Tp065tW4S63Z7xvG6pGVNtEqWWXVOmtWPY0OD7RvF9+Tr79yl+AFkOrw4=&MD=bDVLbTZXUmEyNVZMZTgzSHJIVTE=&TermUrl=https://6ce17629a540.ngrok.io/pg/cybersource/terminal.php'
-        };
-
-        // Request modified... finish sending!
-        interceptedRequest.continue(data);
-    });
-*/
-
-    // Setting request interception to true
-    /*
-    await page.setRequestInterception(true);
-    page.once("request", async interceptedRequest => {
-        console.log('INTERCEPTED Request: ' + interceptedRequest.url());
-        console.log(await interceptedRequest.method());
-        try {
-            await interceptedRequest.continue({
-                method: "POST",
-                postData: "PaReq=eNpVUttuwjAMfc9XoH0AuZQyQCYS141pdFPZkOCtCmYE0RTSFra/X1LoLlEefGzrxOc48LaziOMFqtKihDnmefKBDb3p303LcqpX4UvRbq+EedaL8RO/k/A6iPEk4Yw215mRvMmaAmgNiaOwapeYQkKiTsNZJFsBZ13XcoMEUrSzseSC1Sfkvn5NEzBJinJ6yKw2SUObrU0KVLtGjvasFQKt6gRUVprCfskgbAOtAYHSHuSuKI49Si+XS3N75cEzmiJvqiwF6jsI0N85X0sf5U7+p97I2Cx5nB7W0XuXRfv1ep6G82QZTRb7SR+o7yCwcSNJwZyCe8YbrNNrdXqCAa3yBJLUjyPj3JkTOuFXSODoHxpcEfeVvwmnqLQWjaol1YgAfh4z4wRI59JP7CT8Dj569HarwhkoLsNwO2J5LPRDi4fdwN2AMd5p+xVULRWjdnYJwYKKUlfeUU9Dbwumt7/gon9/5BsuDLU9&MD=RnV1RmlZNU90NjZZMm5MaVNESjE=&TermUrl=https://6ce17629a540.ngrok.io/pg/cybersource/terminal.php",
-                headers: {
-                  ...interceptedRequest.headers(),
-                  "Content-Type": "application/x-www-form-urlencoded"
-                  }
-                });
-        } catch(err) {
-            console.log(err);
-        }
-    });
-    await page.goto('https://netsafe.hdfcbank.com/ACSWeb/com.enstage.entransact.servers.AccessControlServerSSL?ty=V', {
-        timeout: 0,
-        waitUntil: 'networkidle2',
-    })
-    page.on('response', async response => {
-        console.log('INTERCEPTED Response: ' + response.url());
-        console.log(await response.status());
-
-    });
-    */
 
     console.log("payment URL is -> " + payURL);
     // The below works for a GET request
@@ -115,10 +72,44 @@ app.get('/pay-headless', async (req, res) => {
         waitUntil: 'networkidle2',
     })
 
-    var pollURL = "http://localhost:8081/otp?pollURL=" + otpURL
-    console.log("trying to poll OTP from " + pollURL)
-    const response = await fetch(pollURL);
-    const json = await response.json();
+    let count = 0;
+    var pollURL = req.query.pollURL
+    var client = {
+       get: async function () {
+           const response = await fetch(pollURL)
+           const text = await response.text();
+
+           return new Promise(function (resolve, reject) {
+               count ++;
+               setTimeout(function () {
+                   if (text != "1") resolve({status:'DONE',otherStuff:'Other Stuff', otp: text});
+                   else resolve({status: `count: ${count}`});
+               }, 10000);
+           });
+       }
+    }
+     async function someFunction() {
+         while (true) {
+             let dataResult = await client.get('/status');
+             console.log(dataResult.status);
+             if (dataResult.status == "DONE" || count > 10) {
+                 return dataResult;
+             }
+         }
+     }
+     (async () => {
+         //let r = await someFunction();
+         //console.log(r);
+         // send otp back
+     })();
+    let r = await someFunction();
+    console.log(r);
+
+    //var pollURL = "http://localhost:8081/otp?pollURL=" + otpURL
+    //console.log("trying to poll OTP from " + pollURL)
+    //const response = await fetch(pollURL);
+    //const json = await response.json();
+
     const OTP = json.otp;
     console.log("got in main");
     console.log(OTP);
