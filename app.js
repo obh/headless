9e0cf550b16364d37300dce9d25c24e2dabfdc11
Rohit Sharma <rsedwardian@gmail.com>
2020-07-08 20:33:15 +0530

    cleaning code

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express')
+// for post parsing
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 const puppeteer = require('puppeteer');
 const fetch = require("node-fetch");
 const got = require('got');
@@ -7,6 +12,8 @@ const IS_PRODUCTION = 'production123123' === 'production';
 const app = express()
 
 const port = 8081
+
+global.pages = {}
 
 
 const getBrowser = () => IS_PRODUCTION ?
@@ -18,6 +25,71 @@ const getBrowser = () => IS_PRODUCTION ?
     //headless: false,
     //slowMo: 1000 // slow down by 250ms
   });
+
+app.post('/initiate', jsonParser, async(req, res) => {
+    console.log(req.body)
+    var payUrl = req.body.payURL
+    var orderHash = req.body.orderHash
+    try {
+     browser = await getBrowser();
+     const page = await browser.newPage();
+     // how to do a post - https://stackoverflow.com/questions/47060534/how-do-post-request-in-puppeteer
+     //await page.setRequestInterception(true);
+
+
+     console.log("payment URL is -> " + payUrl);
+     // The below works for a GET request
+     await page.goto(payUrl, {
+         timeout: 0,
+         waitUntil: 'networkidle2',
+     })
+     pages[orderHash] = page
+     res.json({status: "OK"})
+    } catch (error) {
+       console.log(error.message);
+       console.log(error);
+     if (!res.headersSent) {
+       res.status(400).send(error.message);
+     }
+   } finally {
+     if (browser) {
+        browser.close();
+     }
+   }
+})
+
+app.post('/submitOTP', jsonParser, async(req, res) => {
+    console.log(req.body)
+    var OTP = req.body.otp
+    var orderHash = req.body.orderHash
+    try {
+    var page = pages[orderHash]
+    console.log("got in main");
+    console.log(OTP);
+    page.$eval('#txtOtpPassword', (el, otp) => el.value = otp, OTP);
+    console.log(typeof(page))
+    //page.click("button[type=submit]");
+
+
+    //await Promise.all([
+    //  page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    //]);
+
+    const screenshot = await page.screenshot();
+    res.end(screenshot, 'binary');
+    } catch (error) {
+        console.log(error.message);
+        console.log(error);
+      if (!res.headersSent) {
+        res.status(400).send(error.message);
+      }
+    } finally {
+      if (browser) {
+         browser.close();
+      }
+    }
+})
+
 
 app.get('/otp', async(req, res) => {
     let count = 0;
@@ -84,7 +156,7 @@ app.get('/pay-headless', async (req, res) => {
                setTimeout(function () {
                    if (text != "1") resolve({status:'DONE', otp: text});
                    else resolve({status: `fetch count: ${count}`});
-               }, 1000);
+               }, 10000);
            });
        }
     }
@@ -93,7 +165,7 @@ app.get('/pay-headless', async (req, res) => {
              let dataResult = await client.get('/status');
              console.log("Fetching data...")
              console.log(dataResult.status);
-             if (dataResult.status == "DONE" || count > 10) {
+             if (dataResult.status == "DONE" || count > 15) {
                  return dataResult;
              }
          }
@@ -106,7 +178,7 @@ app.get('/pay-headless', async (req, res) => {
     //const response = await fetch(pollURL);
     //const json = await response.json();
 
-    const OTP = json.otp;
+    const OTP = r.otp;
     console.log("got in main");
     console.log(OTP);
     page.$eval('#txtOtpPassword', (el, otp) => el.value = otp, OTP);
